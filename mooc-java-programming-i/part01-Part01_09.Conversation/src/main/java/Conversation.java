fbb6894c63c4d774a5b93cf86287db1a6b73e7bc
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-06 09:48:49 -0600

    Add solution for Part01_09 exercise

--- mooc-java-programming-i/part01-Part01_09.Conversation/src/main/java/Conversation.java
+++ mooc-java-programming-i/part01-Part01_09.Conversation/src/main/java/Conversation.java
@@ -7,6 +7,11 @@ public class Conversation {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Greetings! How are you doing?");
+        String firstReply = scanner.nextLine();
+        System.out.println("Oh, how interesting. Tell me more!");
+        String secondReply = scanner.nextLine();
+        System.out.println("Thanks for sharing!");
+        
     }
 }
