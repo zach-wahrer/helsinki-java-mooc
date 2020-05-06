860ede7d64b3d364fe90c510bf56daa43ae67c93
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-06 10:27:32 -0600

    Add solution for Part01_12 exercise

--- mooc-java-programming-i/part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
+++ mooc-java-programming-i/part01-Part01_12.IntegerInput/src/main/java/IntegerInput.java
@@ -7,6 +7,8 @@ public class IntegerInput {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
-
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+        System.out.println("You gave the number " + number);
     }
 }
