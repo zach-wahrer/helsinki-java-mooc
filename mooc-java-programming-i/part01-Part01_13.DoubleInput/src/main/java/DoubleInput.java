41a0695176e21cd5ee4640526999b63916987607
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-06 10:30:15 -0600

    Add solution for Part01_13 exercise

--- mooc-java-programming-i/part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
+++ mooc-java-programming-i/part01-Part01_13.DoubleInput/src/main/java/DoubleInput.java
@@ -7,6 +7,8 @@ public class DoubleInput {
         Scanner scanner = new Scanner(System.in);
 
         // write your program here
-
+        System.out.println("Give a number:");
+        double number = Double.valueOf(scanner.nextLine());
+        System.out.println("You gave the number " + number);
     }
 }
