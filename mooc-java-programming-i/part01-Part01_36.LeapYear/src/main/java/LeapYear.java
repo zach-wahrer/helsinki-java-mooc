e40e6339807c6ed8c57aa607f87a668262da4ed0
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-07 14:04:42 -0600

    Add solution for Part01_36 exercise

--- mooc-java-programming-i/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
+++ mooc-java-programming-i/part01-Part01_36.LeapYear/src/main/java/LeapYear.java
@@ -5,6 +5,13 @@ public class LeapYear {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        System.out.println("Give a year:");
+        int year = Integer.valueOf(scan.nextLine());
+        
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+            System.out.println("This year is a leap year.");          
+        } else {
+            System.out.println("This year is not a leap year.");
+        }
     }
 }
