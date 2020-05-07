0fa8a7741139eebc2f3f72b6fa8ebe787305b30f
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-07 13:17:38 -0600

    Add solution for Part01_33 exercise

--- mooc-java-programming-i/part01-Part01_33.Password/src/main/java/Password.java
+++ mooc-java-programming-i/part01-Part01_33.Password/src/main/java/Password.java
@@ -7,5 +7,13 @@ public class Password {
         Scanner scan = new Scanner(System.in);
 
         // Write your program here 
+        System.out.println("Password?");
+        String password = scan.nextLine();
+        
+        if (password.equals("Caput Draconis")) {
+            System.out.println("Welcome!");
+        } else {
+            System.out.println("Off with you!");
+        }
     }
 }
