7efe93c895982eaabd355f66e14203c4f4890220
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-08 12:32:19 -0600

    Add solution for Part02_18 exercise

--- mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
+++ mooc-java-programming-i/part02-Part02_18.SumOfASequenceTheSequel/src/main/java/SumOfASequenceTheSequel.java
@@ -5,6 +5,18 @@ public class SumOfASequenceTheSequel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("First number?");
+        int firstNumber = Integer.valueOf(scanner.nextLine());
+        System.out.println("Second number?");
+        int secondNumber = Integer.valueOf(scanner.nextLine());
+        
+        int sum = 0;
+        
+        while (firstNumber <= secondNumber) {
+            sum += firstNumber;
+            firstNumber++;
+        }
+        
+        System.out.println("The sum is: " + sum);
     }
 }
