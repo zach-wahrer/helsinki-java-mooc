c68bd340783d4d4618cb059547e5b0663a190722
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-08 12:37:27 -0600

    Add solution for Part02_19 exercise

--- mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
+++ mooc-java-programming-i/part02-Part02_19.Factorial/src/main/java/Factorial.java
@@ -5,6 +5,14 @@ public class Factorial {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Give a number:");
+        int factorialOf = Integer.valueOf(scanner.nextLine());
+        int factorialSum = 1;
+        
+        for (int i = 1; i <= factorialOf; i++) {
+            factorialSum *= i;
+        }
+        
+        System.out.println("Factorial: " + factorialSum);
     }
 }
