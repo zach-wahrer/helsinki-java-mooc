a9cea083d194187a943ec7ab07e3189dbf31f8d9
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-08 10:14:37 -0600

    Add solution for Part02_09 exercise

--- mooc-java-programming-i/part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
+++ mooc-java-programming-i/part02-Part02_09.NumberOfNegativeNumbers/src/main/java/NumberOfNegativeNumbers.java
@@ -5,6 +5,22 @@ public class NumberOfNegativeNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int numOfNegativeNums = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            if (number > 0) {
+                continue;
+            }
+            
+            numOfNegativeNums += 1;
+        }
+        
+        System.out.println("Number of negative numbers: " + numOfNegativeNums);
     }
 }
