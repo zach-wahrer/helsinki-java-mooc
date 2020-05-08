8d563f5e1f6f10de0a9c35e041f7db18513e54bd
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-08 12:14:41 -0600

    Simplify solution for Part02_15 exercise

--- mooc-java-programming-i/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
+++ mooc-java-programming-i/part02-Part02_15.CountingToHundred/src/main/java/CountingToHundred.java
@@ -6,8 +6,9 @@ public class CountingToHundred {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number = Integer.valueOf(scanner.nextLine());
-        for (int i = number; i <= 100; i++) {
-            System.out.println(i);
+        while (number <= 100) {
+            System.out.println(number);
+            number++;
         }
     }
 }
