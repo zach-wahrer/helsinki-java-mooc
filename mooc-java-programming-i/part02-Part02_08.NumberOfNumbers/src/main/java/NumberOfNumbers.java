0811709bbea448b6883aac886bd689bbf48aa984
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-08 10:10:59 -0600

    Add solution for Part02_08 exercise

--- mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_08.NumberOfNumbers/src/main/java/NumberOfNumbers.java
@@ -5,6 +5,18 @@ public class NumberOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int numOfNumbers = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            numOfNumbers += 1;
+        }
+        
+        System.out.println("Number of numbers: " + numOfNumbers);
     }
 }
