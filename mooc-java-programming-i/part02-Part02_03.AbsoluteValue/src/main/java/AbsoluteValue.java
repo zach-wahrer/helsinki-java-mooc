ebabe64d4b88927ffbd0a6fe92bb2e29eb588d27
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-08 09:35:28 -0600

    Add solution for Part02_03 exercise

--- mooc-java-programming-i/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
+++ mooc-java-programming-i/part02-Part02_03.AbsoluteValue/src/main/java/AbsoluteValue.java
@@ -5,6 +5,10 @@ public class AbsoluteValue {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int number = Integer.valueOf(scanner.nextLine());
+        if (number < 0) {
+            number *= -1;
+        }
+        System.out.println(number);
     }
 }
