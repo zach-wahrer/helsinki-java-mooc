556da3206a88bc511e7cee04299fcf936f14b64d
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-08 10:34:46 -0600

    Add solution for Part02_12 exercise

--- mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,6 +5,20 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int numOfNumbers = 0;
+        int sum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            numOfNumbers += 1;
+            sum += number;
+        }
+        
+        System.out.println("Average of the numbers: " + sum / (1.0 * numOfNumbers));
     }
 }
