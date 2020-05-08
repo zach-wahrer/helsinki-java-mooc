61edcaa5825b5cb4d62433612516614a295bbce9
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-08 10:17:43 -0600

    Add solution for Part02_10 exercise

--- mooc-java-programming-i/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_10.SumOfNumbers/src/main/java/SumOfNumbers.java
@@ -5,6 +5,18 @@ public class SumOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int sum = 0;
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                break;
+            }
+            
+            sum += number;
+        }
+        
+        System.out.println("Sum of the numbers: " + sum);
     }
 }
