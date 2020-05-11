a73deebf8fd77726128e1661819db58615d9b2c9
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-11 10:26:46 -0600

    Add solution for Part02_28 exercise

--- mooc-java-programming-i/part02-Part02_28.Word/src/main/java/Word.java
+++ mooc-java-programming-i/part02-Part02_28.Word/src/main/java/Word.java
@@ -5,7 +5,10 @@ public class Word {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println(word());
     }
 
+    public static String word() {
+        return "This method returns a string!";
+    }
 }
