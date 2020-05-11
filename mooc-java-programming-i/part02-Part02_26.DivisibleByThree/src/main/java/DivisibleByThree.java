76168e58debec1fc1a5cace9b9d9a978d6e3c5dc
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-11 09:38:18 -0600

    Add solution for Part02_26 exercise

--- mooc-java-programming-i/part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
+++ mooc-java-programming-i/part02-Part02_26.DivisibleByThree/src/main/java/DivisibleByThree.java
@@ -5,7 +5,16 @@ public class DivisibleByThree {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        divisibleByThreeInRange(2, 10);
+    }
+    
+    public static void divisibleByThreeInRange(int beginning, int end) {
+        while (beginning <= end) {
+            if (beginning % 3 == 0) {
+                System.out.println(beginning);
+            }
+            beginning++;
+        }
     }
 
 }
