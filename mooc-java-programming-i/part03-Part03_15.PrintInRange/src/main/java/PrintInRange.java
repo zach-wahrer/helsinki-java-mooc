76d74251e8983b4f3fb103b86354bc2e36a3544d
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-12 14:25:24 -0600

    Add solution for Part03_15 exercise

--- mooc-java-programming-i/part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
+++ mooc-java-programming-i/part03-Part03_15.PrintInRange/src/main/java/PrintInRange.java
@@ -5,6 +5,22 @@ public class PrintInRange {
 
     public static void main(String[] args) {
         // Try your method here
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(10);
+        nums.add(8);
+        
+        printNumbersInRange(nums, 1, 8);
     }
     
+    public static void printNumbersInRange(ArrayList<Integer> numbers, int lowerLimit, int upperLimit) {
+        System.out.println("The numbers in the range [" + lowerLimit + ", " + upperLimit +"]");
+        for (int number: numbers) {
+            if (number >= lowerLimit && number <= upperLimit) {
+                System.out.println(number);
+            }
+        }
+    }
 }
