bc0eccceff5e9cef29e1732fdfb3d5a7cbfb3a40
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-12 14:37:31 -0600

    Add solution for Part03_15 exercise

--- mooc-java-programming-i/part03-Part03_16.Sum/src/main/java/Sum.java
+++ mooc-java-programming-i/part03-Part03_16.Sum/src/main/java/Sum.java
@@ -5,6 +5,20 @@ public class Sum {
 
     public static void main(String[] args) {
         // Try your method here
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        
+        System.out.println(sum(numbers));
     }
-
+    public static int sum(ArrayList<Integer> numbers) {
+        int sum = 0;
+        for (int number: numbers) {
+            sum += number;
+        }
+        return sum;
+    }
 }
