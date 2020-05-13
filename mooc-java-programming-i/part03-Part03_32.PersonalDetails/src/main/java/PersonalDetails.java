31b23c854399e100093fd3e371984be5afb3eb4d
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-13 11:27:15 -0600

    Add solution for Part03_32 exercise

--- mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ mooc-java-programming-i/part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -6,7 +6,28 @@ public class PersonalDetails {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String longestName = "";
+        int numberOfPeople = 0;
+        int sumOfYears = 0;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] person = input.split(",");
+            if (person[0].length() > longestName.length()) {
+                longestName = person[0];
+            }
+            
+            sumOfYears += Integer.valueOf(person[1]);
+            numberOfPeople++;
+        }
+        
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + 1.0 * sumOfYears / numberOfPeople);
 
     }
 }
