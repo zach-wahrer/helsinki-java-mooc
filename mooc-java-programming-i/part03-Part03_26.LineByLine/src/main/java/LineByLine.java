0ef4df8d0c89b06c9b541ecde8760f884ecf58a7
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-13 10:51:35 -0600

    Add solution for Part03_26 exercise

--- mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
+++ mooc-java-programming-i/part03-Part03_26.LineByLine/src/main/java/LineByLine.java
@@ -5,7 +5,16 @@ public class LineByLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("")) {
+                break;
+            }
+            String[] list = input.split(" ");
+            for (String word: list) {
+                System.out.println(word);
+            }
+        }
 
     }
 }
