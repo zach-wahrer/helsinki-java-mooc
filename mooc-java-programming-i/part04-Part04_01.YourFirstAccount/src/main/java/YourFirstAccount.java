c6074fa5adb02b69d9d04ba0ed2cf23049a6a658
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-13 12:40:09 -0600

    Add solution for Part04_01 exercise

---
+++ mooc-java-programming-i/part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,13 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+        Account testAccount = new Account("Test account", 100.00);
+        testAccount.deposit(20);
+        System.out.println(testAccount);
+        
+                
+    }
+}
