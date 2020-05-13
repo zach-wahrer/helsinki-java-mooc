f1af4ab1eee600c5019f31fd5f0cc2b840c079cf
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-13 11:14:01 -0600

    Add solution for Part03_30 exercise

--- mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -5,7 +5,22 @@ public class AgeOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int oldest = -1;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] person = input.split(",");
+            if (Integer.valueOf(person[1]) > oldest) {
+                oldest = Integer.valueOf(person[1]);
+            }
+        }
+        
+        System.out.println("Age of the oldest: " + oldest);
 
     }
 }
