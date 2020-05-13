f159c832452cd83945adddf89459f6d1f6c9a8b5
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-13 10:07:59 -0600

    Add solution for Part03_23 exercise

--- mooc-java-programming-i/part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
+++ mooc-java-programming-i/part03-Part03_23.PrintThrice/src/main/java/PrintThrice.java
@@ -7,6 +7,8 @@ public class PrintThrice {
         Scanner scanner = new Scanner(System.in);
 
         // Write your program here
-
+        System.out.println("Give a word:");
+        String word = scanner.nextLine();
+        System.out.println(word + word + word);
     }
 }
