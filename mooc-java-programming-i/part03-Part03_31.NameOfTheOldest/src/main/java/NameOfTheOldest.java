23310ebbd349b0ab52a7a8e3bebb0fc1f8899ef6
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-13 11:19:01 -0600

    Add solution for Part03_31 exercise

--- mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ mooc-java-programming-i/part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -5,7 +5,24 @@ public class NameOfTheOldest {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        String nameOfOldest = "";
+        int ageOfOldest = -1;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("")) {
+                break;
+            }
+            
+            String[] person = input.split(",");
+            if (Integer.valueOf(person[1]) > ageOfOldest) {
+                nameOfOldest = person[0];
+                ageOfOldest = Integer.valueOf(person[1]);
+            }
+        }
+        
+        System.out.println("Name of the oldest: " + nameOfOldest);
 
     }
 }
