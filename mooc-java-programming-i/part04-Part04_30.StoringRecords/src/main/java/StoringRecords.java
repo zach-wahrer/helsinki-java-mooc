06195cdcadaad85addb0012da238c3f4b3fd6516
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-15 10:45:49 -0600

    Add solution for Part04_30 exercise

--- mooc-java-programming-i/part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
+++ mooc-java-programming-i/part04-Part04_30.StoringRecords/src/main/java/StoringRecords.java
@@ -25,6 +25,16 @@ public class StoringRecords {
 
         // Write here the code for reading from file
         // and printing the read records
+        try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+            while (fileScanner.hasNextLine()) {
+                String[] line = fileScanner.nextLine().split(",");
+                String name = line[0];
+                int age = Integer.valueOf(line[1]);
+                persons.add(new Person(name, age));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
         return persons;
 
     }
