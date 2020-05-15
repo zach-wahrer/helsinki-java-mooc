fb9baee598faecb1d7965c59d9cddae2dd8be94a
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-15 10:09:13 -0600

    Add solution for Part04_28 exercise

--- mooc-java-programming-i/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
+++ mooc-java-programming-i/part04-Part04_28.NumbersFromAFile/src/main/java/NumbersFromAFile.java
@@ -1,6 +1,7 @@
 
 import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class NumbersFromAFile {
 
@@ -13,7 +14,35 @@ public class NumbersFromAFile {
         int lowerBound = Integer.valueOf(scanner.nextLine());
         System.out.print("Upper bound? ");
         int upperBound = Integer.valueOf(scanner.nextLine());
-
+        
+        ArrayList<Integer> numbers = readFileToArrayList(file);
+        
+        System.out.println("Numbers: " + numsWithinBounds(numbers, upperBound, lowerBound));
+        
+    }
+    
+    public static ArrayList readFileToArrayList(String fileName) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        
+        try (Scanner fileScanner = new Scanner(Paths.get(fileName))) {
+            while (fileScanner.hasNextLine()) {
+                numbers.add(Integer.valueOf(fileScanner.nextLine()));
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+ 
+        return numbers;
+    }
+    
+    public static int numsWithinBounds(ArrayList<Integer> numbers, int upperBound, int lowerBound) {
+        int count = 0;
+        for (int number: numbers) {
+            if (number >= lowerBound && number <= upperBound) {
+                count++;
+            }
+        }
+        return count;
     }
 
 }
