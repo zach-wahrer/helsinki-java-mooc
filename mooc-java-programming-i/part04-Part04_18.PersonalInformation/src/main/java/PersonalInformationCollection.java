9709c1049616b2acb94729c43347d040843c8409
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-15 08:23:10 -0600

    Add solution for Part04_18 exercise

--- mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
+++ mooc-java-programming-i/part04-Part04_18.PersonalInformation/src/main/java/PersonalInformationCollection.java
@@ -9,6 +9,21 @@ public class PersonalInformationCollection {
 
         ArrayList<PersonalInformation> infoCollection = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            String firstName = scanner.nextLine();
+            if (firstName.equals("")) {
+                break;
+            }
+            String lastName = scanner.nextLine();
+            String id = scanner.nextLine();
+            
+            infoCollection.add(new PersonalInformation(firstName, lastName, id));
+        }
+        
+        for (PersonalInformation person: infoCollection) {
+            System.out.println(person.getFirstName() + " " + person.getLastName());
+        }
 
     }
 }
