4390a5047ea382a89698cee259b475cb3883190f
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-15 08:39:41 -0600

    Add solution for Part04_19 exercise

--- mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
+++ mooc-java-programming-i/part04-Part04_19.TelevisionPrograms/src/main/java/Main.java
@@ -8,6 +8,29 @@ public class Main {
 
         ArrayList<TelevisionProgram> programs = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.print("Name: ");
+            String name = scanner.nextLine();
+            
+            if (name.equals("")) {
+                break;
+            }
+            
+            System.out.print("Duration: ");
+            int duration = Integer.valueOf(scanner.nextLine());
+            
+            programs.add(new TelevisionProgram(name, duration));
+        }
+        
+        System.out.print("Program's maximum duration? ");
+        int maxDuration = Integer.valueOf(scanner.nextLine());
+        
+        for (TelevisionProgram show: programs) {
+            if (maxDuration >= show.getDuration()) {
+                System.out.println(show);
+            }
+        }
 
     }
 }
