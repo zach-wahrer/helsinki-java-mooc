77fea4443839dcb9fd88d41c5639f022f1d433e7
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-15 09:42:02 -0600

    Add solution for Part04_26 exercise

--- mooc-java-programming-i/part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
+++ mooc-java-programming-i/part04-Part04_26.GuestListFromAFile/src/main/java/GuestListFromAFile.java
@@ -13,6 +13,14 @@ public class GuestListFromAFile {
 
         ArrayList<String> list = new ArrayList<>();
         // implement reading the file here.
+        try (Scanner fileScanner = new Scanner(Paths.get(file))) {
+            while (fileScanner.hasNextLine()) {
+                list.add(fileScanner.nextLine());
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
         System.out.println("");
 
         System.out.println("Enter names, an empty line quits.");
