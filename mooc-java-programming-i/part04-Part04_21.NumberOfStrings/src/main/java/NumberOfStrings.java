f10aebe2d2717a72e7b550589e51cf95785d2b63
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-15 09:10:35 -0600

    Add solution for Part04_21 exercise

--- mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ mooc-java-programming-i/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,16 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int numOfStrings = 0;
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            
+            numOfStrings++;
+        }
+        System.out.println(numOfStrings);
     }
 }
