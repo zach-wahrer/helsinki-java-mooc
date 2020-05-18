2a085ac42f6c6dfb719a122ab584c836f87f0695
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-18 15:32:12 -0600

    Add solution for Part05_06 exercise

--- mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
+++ mooc-java-programming-i/part05-Part05_06.OverloadedCounter/src/main/java/MainProgram.java
@@ -3,5 +3,15 @@ public class MainProgram {
 
     public static void main(String[] args) {
         // Test your counter here
+        Counter zeroCounter = new Counter();
+        Counter tenCounter = new Counter(10);
+        System.out.println(zeroCounter);
+        System.out.println(tenCounter);
+        
+        zeroCounter.increase();
+        tenCounter.increase(10);
+        
+        System.out.println(zeroCounter);
+        System.out.println(tenCounter);
     }
 }
