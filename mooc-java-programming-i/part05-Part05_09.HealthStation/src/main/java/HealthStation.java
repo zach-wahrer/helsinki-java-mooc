f09de76156872ff4d862260e6c6a3c1106e973a8
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-19 08:27:54 -0600

    Add solution for Part05_09 exercise

--- mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
+++ mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/HealthStation.java
@@ -1,10 +1,25 @@
 
 public class HealthStation {
+    
+    private int timesWeighed;
+    
+    public HealthStation() {
+        timesWeighed = 0;
+    }
 
 
     public int weigh(Person person) {
         // return the weight of the person passed as the parameter
-        return -1;
+        this.timesWeighed += 1;
+        return person.getWeight();
+    }
+    
+    public void feed(Person person) {
+        person.setWeight(person.getWeight() + 1);
+    }
+    
+    public int weighings() {
+        return this.timesWeighed;
     }
 
 }
--- mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/Main.java
+++ mooc-java-programming-i/part05-Part05_09.HealthStation/src/main/java/Main.java
@@ -3,5 +3,14 @@ public class Main {
 
     public static void main(String[] args) {
         // write experimental code here to check how your program functions
+        Person anon = new Person("Anon", 35, 180, 70);
+        HealthStation backAlley = new HealthStation();
+        
+        System.out.println(backAlley.weigh(anon));
+        
+        backAlley.feed(anon);
+        System.out.println(backAlley.weigh(anon));
+        
+        System.out.println(backAlley.weighings());
     }
 }
