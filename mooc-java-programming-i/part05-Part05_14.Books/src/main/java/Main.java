8637567ad211daa134b6dfb4787dd949684782f5
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-19 10:53:16 -0600

    Add solution for Part05_14 exercise

--- mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Main.java
+++ mooc-java-programming-i/part05-Part05_14.Books/src/main/java/Main.java
@@ -18,8 +18,10 @@ public class Main {
             System.out.println("Publication year:");
             int publicationYear = Integer.valueOf(scanner.nextLine());
             Book book = new Book(name, publicationYear);
-            books.add(book);
-
+            
+            if (!(books.contains(book))) {
+                books.add(book);
+            }
         }
         
         // NB! Don't alter the line below!
