d8e3b8744ed3a42bb969c32c485d574c06d1afb7
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-19 11:15:41 -0600

    Add solution for Part05_15 exercise

--- mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Main.java
+++ mooc-java-programming-i/part05-Part05_15.Archive/src/main/java/Main.java
@@ -6,7 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        ArrayList<Item> archive = new ArrayList<>();
+        
+        while (true) {
+            System.out.println("Identifier? (empty will stop)");
+            String id = scanner.nextLine();
+            
+            if (id.equals("")) {
+                break;
+            }
+            
+            System.out.println("Name? (empty will stop)");
+            String name = scanner.nextLine();
+            
+            if (name.equals("")) {
+                break;
+            }
+            
+            Item item = new Item(id, name);
+            
+            if (!(archive.contains(item))) {
+                archive.add(new Item(id, name));
+            }
+        }
+        
+        for (Item item: archive) {
+            System.out.println(item);
+        }
 
     }
 }
