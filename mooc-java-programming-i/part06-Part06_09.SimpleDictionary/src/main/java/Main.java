a1c236e7a373d621a26d7f8a3ed2bafed6e2a063
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-20 14:18:28 -0600

    Add solution for Part06_09 exercise

--- mooc-java-programming-i/part06-Part06_09.SimpleDictionary/src/main/java/Main.java
+++ mooc-java-programming-i/part06-Part06_09.SimpleDictionary/src/main/java/Main.java
@@ -6,5 +6,10 @@ public class Main {
     public static void main(String[] args) {
         // Try out how the different parts of the program work together
         // here
+        SimpleDictionary dictionary = new SimpleDictionary();
+        Scanner scanner = new Scanner(System.in);
+        TextUI userInterface = new TextUI(scanner, dictionary);
+        userInterface.start();
+        
     }
 }
---
+++ mooc-java-programming-i/part06-Part06_09.SimpleDictionary/src/main/java/TextUI.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zachtheclimber
+ */
+import java.util.Scanner;
+
+public class TextUI {
+    private Scanner scanner;
+    private SimpleDictionary dictionary;
+    
+    public TextUI(Scanner scanner, SimpleDictionary dictionary) {
+        this.scanner = scanner;
+        this.dictionary = dictionary;
+    }
+    
+    public void start() {
+        while (true) {
+            System.out.println("Command:");
+            String command = this.scanner.nextLine();
+            
+            if (command.equals("end")) {
+                System.out.println("Bye bye!");
+                break;
+            } else if (command.equals("add")) {
+                System.out.println("Word:");
+                String word = this.scanner.nextLine();
+                System.out.println("Translation:");
+                String translation = this.scanner.nextLine();
+                dictionary.add(word, translation);
+            } else if (command.equals("search")) {
+                System.out.println("To be translated:");
+                String wordToTranslate = this.scanner.nextLine();
+                String translated = dictionary.translate(wordToTranslate);
+                if (translated != null) {
+                    System.out.println("Translation: " + translated);
+                } else {
+                    System.out.println("Word " + wordToTranslate + " was not found");
+                }
+                
+            } else {
+                System.out.println("Unknown command");
+            }
+        }
+    }
+    
+}
