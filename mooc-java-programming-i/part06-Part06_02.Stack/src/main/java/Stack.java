a9a29673394348bb147fb5af2b9bb162f3ea48f8
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-20 07:45:15 -0600

    Add solutino for Part06_02 exercise

---
+++ mooc-java-programming-i/part06-Part06_02.Stack/src/main/java/Stack.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zachtheclimber
+ */
+import java.util.ArrayList;
+
+public class Stack {
+    private ArrayList<String> stackData;
+    
+    public Stack() {
+        this.stackData = new ArrayList<>();
+    }
+    
+    public boolean isEmpty() {
+        return stackData.isEmpty();
+    }
+    
+    public void add(String value) {
+        this.stackData.add(value);
+    }
+    
+    public ArrayList<String> values() {
+        return this.stackData;
+    }
+    
+    public String take() {
+        String popValue = this.stackData.get(this.stackData.size() - 1); 
+        this.stackData.remove(this.stackData.size() - 1);
+        return popValue;
+        
+    }
+}
