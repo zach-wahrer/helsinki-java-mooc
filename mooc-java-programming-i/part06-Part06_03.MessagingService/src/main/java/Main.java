62139d80a6aa993683d1857d3c2b50554ca3679e
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-20 08:27:36 -0600

    Add solution for Part06_03 exercise

--- mooc-java-programming-i/part06-Part06_03.MessagingService/src/main/java/Main.java
+++ mooc-java-programming-i/part06-Part06_03.MessagingService/src/main/java/Main.java
@@ -4,5 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
         // Try out your class here
+        MessagingService service = new MessagingService();
+        
+        Message message1 = new Message("Zach", "This is only a test.");
+        Message message2 = new Message("anon", "this is another test");
+        
+        service.add(message1);
+        System.out.println(service.getMessages());
+        
+        service.add(message2);
+        
+        for (Message message: service.getMessages()) {
+            System.out.println(message);
+        }
     }
 }
