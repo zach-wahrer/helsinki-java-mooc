2bd559c073e374e8d304a568b50dfd7fab296843
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-21 09:16:04 -0600

    Add missing file commit for Part06_10 exercise

---
+++ mooc-java-programming-i/part06-Part06_10.TodoList/src/main/java/ToDoList.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author zachtheclimber
+ */
+import java.util.ArrayList;
+
+public class ToDoList {
+    private ArrayList<String> tasks;
+    
+    public ToDoList() {
+        this.tasks = new ArrayList<>();
+    }
+    
+    public void add(String task) {
+        this.tasks.add(task);
+    }
+    
+    public void print() {
+        for (int i = 0; i < this.tasks.size(); i++) {
+            System.out.println((i + 1) + ": " + this.tasks.get(i));
+        }
+    }
+    
+    public void remove(int number) {
+        this.tasks.remove(number - 1);
+    }
+}
