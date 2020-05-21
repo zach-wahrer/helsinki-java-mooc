f33968e29dc74c13ab734d77297492532ff716c8
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-21 11:22:50 -0600

    Add solution for Part06_13 exercises

--- mooc-java-programming-i/part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
+++ mooc-java-programming-i/part06-Part06_13.Exercises/src/main/java/ExerciseManagement.java
@@ -1,4 +1,48 @@
+import java.util.ArrayList;
 
 public class ExerciseManagement {
+    private ArrayList<Exercise> exercises;
 
+    
+    public ExerciseManagement() {
+        this.exercises = new ArrayList<>();
+    }
+    
+    public ArrayList<String> exerciseList() {
+        ArrayList<String> list = new ArrayList<>();
+        for (Exercise exercise : this.exercises) {
+            list.add(exercise.getName());
+        }
+        return list;
+    }
+    
+    public void add(String exercise) {
+        this.exercises.add(new Exercise(exercise));
+    }
+    
+    public void remove(String exercise) {
+        for (int i = 0; i < this.exercises.size(); i++) {
+            if (this.exercises.get(i).getName().equals(exercise)) {
+                this.exercises.remove(i);
+            }
+        }
+    }
+    
+    public void markAsCompleted(String exercise) {
+        for (Exercise ex : this.exercises) {
+            if (ex.getName().equals(exercise)) {
+                ex.setCompleted(true);
+            }
+        }
+    }
+    
+    public boolean isCompleted(String exercise) {
+        for (Exercise ex : this.exercises) {
+            if (ex.getName().equals(exercise)) {
+                return ex.isCompleted();
+            }
+        }
+        return false;
+    }
+    
 }
--- mooc-java-programming-i/part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
+++ mooc-java-programming-i/part06-Part06_13.Exercises/src/test/java/ExerciseManagementTest.java
@@ -1,6 +1,61 @@
-
-
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import org.junit.Test;
+import org.junit.Before;
 
 public class ExerciseManagementTest {
-
+    
+    private ExerciseManagement management;
+    
+    @Before
+    public void initialize() {
+        management = new ExerciseManagement();
+    }
+    
+    @Test
+    public void exerciseListEmptyAtBeginning() {
+        assertEquals(0, management.exerciseList().size());
+    }
+    
+    @Test
+    public void addingExerciseGrowsListByOne() {
+        management.add("Write a test");
+        assertEquals(1, management.exerciseList().size());
+    }
+    
+    @Test
+    public void addedExerciseIsInList() {
+        management.add("Write a test");
+        assertTrue(management.exerciseList().contains("Write a test"));
+    }
+    
+    @Test
+    public void exerciseCanBeMarkedAsCompleted() {
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        assertTrue(management.isCompleted("New exercise"));
+    }
+    
+    @Test
+    public void ifNotMarkedCompletedIsNotCompleted() {
+        management.add("New exercise");
+        management.markAsCompleted("New exercise");
+        assertFalse(management.isCompleted("Some exercise"));
+    }
+    
+    @Test
+    public void removingExerciseShrinksListByOne() {
+        management.add("Write a test");
+        management.add("Don't remove this");
+        management.remove("Write a test");
+        assertEquals(1, management.exerciseList().size());
+    }
+    
+    @Test
+    public void removedExerciseNotOnList() {
+        management.add("Write a test");
+        management.remove("Write a test");
+        assertFalse(management.exerciseList().contains("Write a test"));
+    }
 }
