d973a8f4b7e69211d94a6c5687718b49dc6bb2cd
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-24 17:57:25 -0600

    Add solution for Part08_01 exercise

--- mooc-java-programming-ii/part08-Part08_01.Cubes/src/main/java/Cubes.java
+++ mooc-java-programming-ii/part08-Part08_01.Cubes/src/main/java/Cubes.java
@@ -5,6 +5,20 @@ public class Cubes {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.equals("end")) {
+                break;
+            }
+            
+            printCube(Integer.valueOf(input));
+            
+        }
+    }
+    
+    public static void printCube(int number) {
+        System.out.println((int) Math.pow(number, 3));
     }
 }
