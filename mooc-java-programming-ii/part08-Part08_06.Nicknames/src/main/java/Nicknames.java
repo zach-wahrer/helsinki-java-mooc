7ddbe9b1fcfbf62f08fb6cbf5810685253205eab
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-24 18:20:17 -0600

    Add solution for Part08_06 exercise

--- mooc-java-programming-ii/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ mooc-java-programming-ii/part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -5,6 +5,14 @@ public class Nicknames {
 
     public static void main(String[] args) {
         // Do the operations required here!
+        HashMap<String, String> nicknames = new HashMap<>();
+        
+        nicknames.put("matthew", "matt");
+        nicknames.put("michael", "mix");
+        nicknames.put("arthur", "artie");
+        
+        System.out.println(nicknames.get("matthew"));
+        
     }
 
 }
