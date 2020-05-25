0555d28b6b14328fd5cd008373afd03f495ea17b
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-24 18:14:39 -0600

    Add solutions for redundant exercises in Part08 (03-05)

--- mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -5,16 +5,45 @@ public class LiquidContainers {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        int container1 = 0;
+        int container2 = 0;
 
         while (true) {
+            System.out.println("First: " + container1 + "/100");
+            System.out.println("Second: " + container2 + "/100");
             System.out.print("> ");
-
             String input = scan.nextLine();
-            if (input.equals("quit")) {
+            String[] commandParts = input.split(" ");
+            
+            if (commandParts[0].equals("quit")) {
                 break;
+            } else if (commandParts[0].equals("add")) {
+                int toAdd = Integer.valueOf(commandParts[1]);
+                if (container1 + toAdd > 100 && toAdd > 0) {
+                    container1 = 100;
+                } else if (toAdd > 0) {
+                    container1 += toAdd;
+                }
+            } else if (commandParts[0].equals("move")) {
+                int toMove = Integer.valueOf(commandParts[1]);
+                if (toMove > container1) {
+                    toMove = container1;
+                    container1 = 0;
+                } else {
+                    container1 -= toMove;
+                }
+                if (toMove + container2 > 100) {
+                    container2 = 100;
+                } else {
+                    container2 += toMove;
+                }
+            } else if (commandParts[0].equals("remove")) {
+                int toRemove = Integer.valueOf(commandParts[1]);
+                if (toRemove > container2) {
+                    toRemove = container2;
+                }
+                container2 -= toRemove;
             }
-
         }
     }
 
--- mooc-java-programming-ii/part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
+++ mooc-java-programming-ii/part08-Part08_04.LiquidContainers2/src/main/java/LiquidContainers2.java
@@ -5,16 +5,28 @@ public class LiquidContainers2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        Container container1 = new Container();
+        Container container2 = new Container();
 
         while (true) {
+            System.out.println("First: " + container1);
+            System.out.println("Second: " + container2);
             System.out.print("> ");
-
             String input = scan.nextLine();
-            if (input.equals("quit")) {
+            String[] commandParts = input.split(" ");
+            
+            if (commandParts[0].equals("quit")) {
                 break;
+            } else if (commandParts[0].equals("add")) {
+                int toAdd = Integer.valueOf(commandParts[1]);
+                container1.add(toAdd);
+            } else if (commandParts[0].equals("move")) {
+                int toMove = Integer.valueOf(commandParts[1]);
+                container1.move(toMove, container2);
+            } else if (commandParts[0].equals("remove")) {
+                int toRemove = Integer.valueOf(commandParts[1]);
+                container2.remove(toRemove);
             }
-
         }
     }
 
