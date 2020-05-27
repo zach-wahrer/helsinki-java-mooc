d27bc58188781117a4cf66ce65c1b1b717a9bc18
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-27 11:48:39 -0600

    Add solution for Part09_02 exercise

--- mooc-java-programming-ii/part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
+++ mooc-java-programming-ii/part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
@@ -1,8 +1,30 @@
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your test code here
+        Student zach = new Student("Zach Wahrer", "A Street, Bozeman, MT 59715");
+        System.out.println(zach);
+        zach.study();
+        zach.study();
+        System.out.println(zach);
+        
+        Teacher twoie = new Teacher("Twoie Wahrer", "B Street, Bozeman, MT 59715", 10000);
+        System.out.println(twoie);
+        
+        ArrayList<Person> people = new ArrayList<>();
+        people.add(zach);
+        people.add(twoie);
+        
+        printPersons(people);
+        
+    }
+    
+    public static void printPersons(ArrayList<Person> persons) {
+        for (Person person : persons) {
+            System.out.println(person);
+        }
     }
 
 }
