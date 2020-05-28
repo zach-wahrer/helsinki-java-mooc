57d2553ed51fd385f746a9e0e43650b50a5a334f
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-28 08:33:33 -0600

    Add solution for Part09_05 exercise

--- mooc-java-programming-ii/part09-Part09_05.TacoBoxes/src/main/java/Main.java
+++ mooc-java-programming-ii/part09-Part09_05.TacoBoxes/src/main/java/Main.java
@@ -3,5 +3,18 @@ public class Main {
 
     public static void main(String[] args) {
         // Test your code here!
+        TripleTacoBox tripleTacos = new TripleTacoBox();
+        System.out.println(tripleTacos.tacosRemaining());
+        tripleTacos.eat();
+        tripleTacos.eat();
+        System.out.println(tripleTacos.tacosRemaining());
+        tripleTacos.eat();
+        tripleTacos.eat();
+        System.out.println(tripleTacos.tacosRemaining());
+        
+        CustomTacoBox oneTaco = new CustomTacoBox(1);
+        oneTaco.eat();
+        System.out.println(oneTaco.tacosRemaining());
+        
     }
 }
