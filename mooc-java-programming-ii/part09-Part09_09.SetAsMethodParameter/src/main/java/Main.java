1c199e2b349da5452065bb613a346f8d6d1fc111
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-28 11:03:23 -0600

    Add solution for Part09_09 exercise

--- mooc-java-programming-ii/part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
+++ mooc-java-programming-ii/part09-Part09_09.SetAsMethodParameter/src/main/java/Main.java
@@ -1,13 +1,21 @@
-
+import java.util.Set;
+import java.util.HashSet;
 
 public class Main {
 
     public static void main(String[] args) {
         // You can test your method here
-
+        HashSet<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(1);
+        set.add(10);
+        set.add(30);
+        System.out.println(returnSize(set));
     }
 
     // implement the method returnSize here, which returns
     // the number of elements in the set that it receives as a parameter.
-
+    public static int returnSize(Set set) {
+        return set.size();
+    }
 }
