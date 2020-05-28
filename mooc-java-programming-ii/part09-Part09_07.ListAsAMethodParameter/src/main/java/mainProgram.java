a29d81ff1a6d0908ba1394293351e2dc0d006675
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-28 10:45:13 -0600

    Add solution for Part09_07 exercise

--- mooc-java-programming-ii/part09-Part09_07.ListAsAMethodParameter/src/main/java/mainProgram.java
+++ mooc-java-programming-ii/part09-Part09_07.ListAsAMethodParameter/src/main/java/mainProgram.java
@@ -1,12 +1,20 @@
-
+import java.util.List;
+import java.util.ArrayList;
 
 public class mainProgram {
 
     public static void main(String[] args) {
         // test your method here
-
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(10);
+        numbers.add(19);
+        System.out.println(returnSize(numbers));
     }
-
+    
+    public static int returnSize(List list) {
+        return list.size();
+    } 
     // Implement here a method returnSize
     // which returns the size of the list given to it
     //as a parameter
