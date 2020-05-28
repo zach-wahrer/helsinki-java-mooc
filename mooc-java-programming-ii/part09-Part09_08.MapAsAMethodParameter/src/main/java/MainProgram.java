3218ff0043e3695451211db47597e33dd1acf654
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-28 10:59:48 -0600

    Add solution for Part09_08 exercise

--- mooc-java-programming-ii/part09-Part09_08.MapAsAMethodParameter/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part09-Part09_08.MapAsAMethodParameter/src/main/java/MainProgram.java
@@ -1,12 +1,19 @@
-
+import java.util.Map;
+import java.util.HashMap;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         // test your method here
-
+        Map<Integer, Boolean> truth = new HashMap<>(); 
+        truth.put(1, false);
+        truth.put(2, true);
+        System.out.println(returnSize(truth));
     }
 
     // Implement here a method returnSize, which takes a Map-object as a parameter 
     // and returns the size of the map object
+    public static int returnSize(Map map) {
+        return map.size();
+    }
 }
