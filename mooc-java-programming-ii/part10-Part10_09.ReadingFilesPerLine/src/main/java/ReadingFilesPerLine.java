0b32f23d375bf67200ccf096ff3d8ce339348a0b
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-29 13:30:17 -0600

    Add solution for Part10_09 exercise

--- mooc-java-programming-ii/part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ mooc-java-programming-ii/part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -13,5 +13,18 @@ public class ReadingFilesPerLine {
         // test the method here
 
     }
-
+    
+    public static List<String> read(String file) {
+        List<String> rows = new ArrayList<>();
+        
+        try {
+            Files.lines(Paths.get(file)).forEach(row -> rows.add(row));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
+        return rows;
+    }
+        
+        
 }
