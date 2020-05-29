9a3e48b8e3f06a46d9a748f3e340ed7892e9e8c5
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-29 10:12:11 -0600

    Add solution for Part10_02 exercise

--- mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ mooc-java-programming-ii/part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -7,6 +7,42 @@ public class AverageOfSelectedNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // toteuta ohjelmasi tänne
-
+        ArrayList<Integer> numbers = getNumbers(scanner);
+        String type = getAverageType(scanner);
+        double average = getAverage(type, numbers);
+        print(average);
+    }
+    
+    public static String getAverageType(Scanner scanner) {
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        return scanner.nextLine();
+    }
+    
+    public static ArrayList<Integer> getNumbers(Scanner scanner) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        System.out.println("Input numbers, type \"end\" to stop.");
+        
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("end")) {
+                break;
+            }
+            
+            numbers.add(Integer.valueOf(input));
+        }
+        
+        return numbers;
+    }
+    
+    public static double getAverage(String type, ArrayList<Integer> numbers) {
+        if (type.equals("n")) {
+            return numbers.stream().mapToInt(s -> s).filter(number -> number < 0).average().getAsDouble();
+        } else {
+            return numbers.stream().mapToInt(s -> s).filter(number -> number > 0).average().getAsDouble();
+        }
+    }
+    
+    public static void print(Object value) {
+        System.out.println(value);
     }
 }
