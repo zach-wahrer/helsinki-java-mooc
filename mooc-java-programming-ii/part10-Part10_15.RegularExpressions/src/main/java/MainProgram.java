98cafd1b2b66aab02517d8a290c0bb28c6fb695e
Zach Wahrer <zachtheclimber@gmail.com>
2020-05-30 17:33:56 -0600

    Add solution for Part10_15 exercise

--- mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ mooc-java-programming-ii/part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -5,5 +5,15 @@ public class MainProgram {
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+        
+        Checker checker = new Checker();
+        
+        System.out.println(checker.isDayOfWeek("mon"));
+        System.out.println(checker.isDayOfWeek("rah"));
+        
+        System.out.println(checker.allVowels("oi"));
+        System.out.println(checker.allVowels("queue"));
+
     }
+   
 }
